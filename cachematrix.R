1123c86d0920a5f6475496454bbff86966660fe3
Monica Guerra <monipao@gmail.com>
2015-03-21 18:11:31 -0500

    First commit - test cycle

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,7 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  #prueba git1
 
 }
 
@@ -12,4 +13,5 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  #prueba git2
 }
