24969aff863a52702c067f8f3ea95f66d2e93f50
Monica Guerra <monipao@gmail.com>
2015-03-21 20:51:45 -0500

    Final version - assigment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Last modified: 21/03/2015
+## Author: Monica Guerra
+## Rutina para crear una matriz, que primero valida si la matriz inversa, ya fue invertida antes, y esta en cache
 
-## Write a short comment describing this function
+## Function call: cacheSolve(makeCacheMatrix(h8))
 
+## Devuelve un vector donde cada elemento es una funcion
 makeCacheMatrix <- function(x = matrix()) {
-  #prueba git1.1
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve1) m <<- solve1
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 
 }
 
 
-## Write a short comment describing this function
-
+## Esta función invierte una matriz, de un vector creado con la funcion "makeCacheMatrix"
+## primero chequea si el calculo se hizo y luego si las matrices son las mismas
+## Si ya fue calculada la matriz inversa y es la misma, entonces no gasta recursos de computo
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  #prueba git2.1
+  m <- x$getsolve()
+  if(!is.null(m)) 
+    if(x$setsolve() == x$getsolve()) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
+
